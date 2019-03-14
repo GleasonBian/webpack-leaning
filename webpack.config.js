16b69929b0d566e04f4f49fa39ce4ddbc3f3a9dc
Gleason <bianliuzhu@gmail.com>
2019-03-14 19:46:33 +0800

    安装 css loader 和 style loaser

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,13 @@ module.exports = {
     filename: 'bundle.js', 
     path: path.resolve(__dirname, 'dist')
   },
+  // npm install --save-dev style-loader css-loader  执行 这个 安装 
   module: {
     rules: [
       {
         test: /\.css$/, 
         use: [
-          'style-loader',
+          'style-loader', //webpack 根据正则表达式， 来确定应该查找哪些文件， 并将其提供给指定的 loader。 在这种情况下， 以.css 结尾的全部文件， 都将被提供给 style - loader 和 css - loader。
           'css-loader'
         ]
       }
