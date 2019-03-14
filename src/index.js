1e30b628b8f6a6937914ca98fef47189b1ddd37d
Gleason <bianliuzhu@gmail.com>
2019-03-14 20:19:08 +0800

    处理 图片

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 
-import _ from 'lodash';
-import './style.css'; // 引入css 文件 
+import _ from 'lodash';//引入咯大师
+import './style.css'; // 引入css 文件
+import Icon from './icon.png' 
 
 function component() { 
   let element = document.createElement('div');
 
   //  Lodash, now imported by this script
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
-  element.classList.add('hello'); // 使用 引入 的 css 文件 
+  
+  // 使用 引入 的 css 文件
+  element.classList.add('hello'); 
+  
+  // 将图像添加到我们现有的 div.
+  let myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,11 @@ module.exports = {
     filename: 'bundle.js', 
     path: path.resolve(__dirname, 'dist')
   },
-  // npm install --save-dev style-loader css-loader  执行 这个 安装 
+  /**
+   * 1. npm install --save-dev style - loader css - loader 安装 css loader 处理 css 文件
+   * 2. npm install --save-dev file-loader 安装 file loader 处理 图片
+   * 3. file - loader 和 url - loader 可以接收并加载任何文件， 然后将其输出到构建目录 使用他们处理 字体文件
+   */
   module: {
     rules: [
       {
@@ -14,6 +18,18 @@ module.exports = {
         use: [
           'style-loader', //webpack 根据正则表达式， 来确定应该查找哪些文件， 并将其提供给指定的 loader。 在这种情况下， 以.css 结尾的全部文件， 都将被提供给 style - loader 和 css - loader。
           'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        text: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
         ]
       }
     ]
