641e768a847451278423f6101c6cadd532322108
Gleason <bianliuzhu@gmail.com>
2019-03-14 19:54:01 +0800

    在 index.js 中 添加 注释

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 
 import _ from 'lodash';
-import './style.css';
+import './style.css'; // 引入css 文件 
 
 function component() { 
   let element = document.createElement('div');
 
   //  Lodash, now imported by this script
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  
+  element.classList.add('hello'); // 使用 引入 的 css 文件 
+
   return element;
 }
 
